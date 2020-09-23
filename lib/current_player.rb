cf0044cf7ba035f317aa9e6aaa2a734999aeff82
Beksultan <beksultankaiypov@gmail.com>
2020-09-23 00:20:04 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,9 +3,9 @@ def turn_count(board)
   board.each do |turns|
    if turns == "X" || "O" 
      counter +=1 
-   end 
+   end
+   return counter 
   end
-  return counter 
 end
 
 def current_player(board)
